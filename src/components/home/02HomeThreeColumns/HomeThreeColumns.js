bbcb28e5628e6a74176b5faaafc4c0a9f30330b1
Paulina Dziobek <dziovek@gmail.com>
2020-04-02 16:52:07 +0200

    HomeThreeColumns component

--- src/components/home/02HomeThreeColumns/HomeThreeColumns.js
+++ src/components/home/02HomeThreeColumns/HomeThreeColumns.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './style.scss';
 
 export default function HomeThreeColumns() {
     return (
-        <>
-            <div>Welcome in HomeThreeColumns</div>
-        </>
+        <section className='columns-container'>
+
+            <div className='column'>
+                <p className='three-columns-number'>10</p>
+                <p className='three-columns-title'>oddanych worków</p>
+                <p className='three-columns-txt'>Oat cake donut cake tootsie roll oat cake dessert. Sugar plum dessert macaroon liquorice I love cupcake candy canes tootsie roll.</p>
+            </div>
+
+            <div className='column'>
+                <p className='three-columns-number'>5</p>
+                <p className='three-columns-title'>wspartych organizacji</p>
+                <p className='three-columns-txt'>Oat cake donut cake tootsie roll oat cake dessert. Sugar plum dessert macaroon liquorice I love cupcake candy canes tootsie roll.</p>
+            </div>
+
+            <div className='column'>
+                <p className='three-columns-number'>7</p>
+                <p className='three-columns-title'>zorganizowanych zbiórek</p>
+                <p className='three-columns-txt'>Oat cake donut cake tootsie roll oat cake dessert. Sugar plum dessert macaroon liquorice I love cupcake candy canes tootsie roll.</p>
+            </div>
+
+        </section>
     )
 };
